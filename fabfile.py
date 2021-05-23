1eb2fe559ca1c8984b26106ff01538205e77ffb1
Mauro Quinteros <quinterosmauro0599@gmail.com>
2021-05-23 15:56:40 -0500

    feat: Update navbar

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,25 @@
-from fabric.api import run, sudo, task, put, get
+from fabric.api import run, sudo, task, put, get, cd, prefix, local, env
 
+env.hosts = ['165.22.47.187']
+env.user = 'mauro'
+env.key_filename = '~/.ssh/id_ed25519.pub'
+
+def deploy_pull():
+    run('git pull')
+
+def install():
+    run('pip install -r requirements.txt')
+
+@task
+def deploy():
+    with cd('python-web'):
+        deploy_pull()
+        with prefix('source env/bin/activate'):
+            install()
+        sudo('systemctl restart nginx')
+
+
+@task
 def show_dir():
     run('ls -la')
 
@@ -31,4 +51,16 @@ def get_txt_file(file):
 
 @task
 def pull():
-    run('cd ./python-web && git pull')
+    # run('cd ./python-web && git pull')
+    with cd('python-web'):
+        run('git pull')
+
+@task
+def install_requirements():
+    with cd('python-web'):
+        with prefix('source env/bin/activate'):
+            run('pip install -r requirements.txt')
+
+@task
+def show_local_dir():
+    local('ls -la')
